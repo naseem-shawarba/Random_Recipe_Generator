1378026d64e22595f93124370a04589fb9df174f
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-26 13:33:07 +0200

    deleted unnecessary pages, components and logos

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-app-bar app>
       <v-toolbar-title>
         <a href="/" class="title">{{ title }}</a>
       </v-toolbar-title>
@@ -33,26 +14,6 @@ export default {
   name: "DefaultLayout",
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: "mdi-home",
-          title: "Home",
-          to: "/",
-        },
-        {
-          icon: "mdi-file-document",
-          title: "Documentation",
-          to: "/documentation",
-        },
-        {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire",
-        },
-      ],
       title: "Random Recipe Generator",
     };
   }
